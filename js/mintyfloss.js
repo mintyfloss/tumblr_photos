054b90b6a9d74b0cd62f4899f50c6f3d5b410daf
Michael <mintyfloss@gmail.com>
2013-01-28 00:54:21 -0500

    - removing bootstrap responsive css - creating namespace for the JS

--- js/mintyfloss.js
+++ js/mintyfloss.js
@@ -1,8 +1,9 @@
-(function($){
+var mfTumblr = mfTumblr || {};
+
 // *****************
 //  MODELS
 // *****************
-	var Post = Backbone.Model.extend({
+	mfTumblr.Post = Backbone.Model.extend({
 		defaults: {
 			id: '0',
 			postUrl: '',
@@ -11,13 +12,13 @@
 		}
 	});
 
-	var Header = Backbone.Model.extend({
+	mfTumblr.Header = Backbone.Model.extend({
 		defaults: {
 			showFullHeader: true
 		}
 	});
 
-	var FullWindow = Backbone.Model.extend({
+	mfTumblr.FullWindow = Backbone.Model.extend({
 		defaults: {
 			scrollAmount: 0
 		}
@@ -26,20 +27,20 @@
 // *****************
 //  COLLECTIONS
 // *****************
-	var Posts = Backbone.Collection.extend({
-		model: Post
+	mfTumblr.Posts = Backbone.Collection.extend({
+		model: mfTumblr.Post
 	});
 
 // *****************
 //  VIEWS
 // *****************
-	var PostsView = Backbone.View.extend({
+	mfTumblr.PostsView = Backbone.View.extend({
 		el: $('.posts'),
 
 		initialize: function() {
 			_.bindAll(this, 'render');
 
-			this.collection = new Posts();
+			this.collection = new mfTumblr.Posts();
 
 			var $posts = this.$el.find('.post');
 
@@ -52,7 +53,7 @@
 			_($posts).each(function(postElement){
 				var
 					$postElement = $(postElement),
-					post = new Post({
+					post = new mfTumblr.Post({
 						id: $postElement.attr('data-post-id'),
 						postUrl: $postElement.attr('data-post-url'),
 						type: $postElement.attr('data-post-type'),
@@ -62,7 +63,7 @@
 
 				view.collection.add(post);
 
-				new PostView({
+				new mfTumblr.PostView({
 					el: $postElement,
 					model: post
 				});
@@ -70,31 +71,31 @@
 		}
 	});
 
-	var PostView = Backbone.View.extend({
+	mfTumblr.PostView = Backbone.View.extend({
 		initialize: function() {
-			var
-				$triangle = this.$el.find('.triangle'),
-				canvas = $triangle.get(0)
-			;
+			// var
+			// 	$triangle = this.$el.find('.triangle'),
+			// 	canvas = $triangle.get(0)
+			// ;
 
-			if (canvas && canvas.getContext) {
-				var context = canvas.getContext('2d');
+			// if (canvas && canvas.getContext) {
+			// 	var context = canvas.getContext('2d');
 
-				if (context) {
-					context.beginPath();
+			// 	if (context) {
+			// 		context.beginPath();
 
-					// Start from the top-left point.
-					context.lineTo(50, 50); // give the (x,y) coordinates
-					context.lineTo(25, 25);
-					context.lineTo(50, 0);
-					context.lineTo(50, 50);
+			// 		// Start from the top-left point.
+			// 		context.lineTo(50, 50); // give the (x,y) coordinates
+			// 		context.lineTo(25, 25);
+			// 		context.lineTo(50, 0);
+			// 		context.lineTo(50, 50);
 
-					// Done! Now fill the shape, and draw the stroke.
-					// Note: your shape will not be visible until you call any of the two methods.
-					context.fill();
-					context.closePath();
-				}
-			}
+			// 		// Done! Now fill the shape, and draw the stroke.
+			// 		// Note: your shape will not be visible until you call any of the two methods.
+			// 		context.fill();
+			// 		context.closePath();
+			// 	}
+			// }
 		},
 
 		events: {
@@ -104,7 +105,7 @@
 		}
 	});
 
-	var HeaderView = Backbone.View.extend({
+	mfTumblr.HeaderView = Backbone.View.extend({
 		el: $('#header'),
 
 		initialize: function() {
@@ -117,17 +118,21 @@
 
 		render: function() {
 			this.$el.find('.navbar').toggleClass('navbar-fixed-top', !this.model.get('showFullHeader'));
+
+			this.$el.find('#toTop').click(function() {
+				$('html:not(:animated), body:not(:animated)').animate({ scrollTop: 0 }, 'slow');
+			});
 		}
 	});
 
-	var FullWindowView = Backbone.View.extend({
+	mfTumblr.FullWindowView = Backbone.View.extend({
 		el: $(window),
 
 		initialize: function() {
 			_.bindAll(this, 'detect_scroll');
 
-			// bind to window
-			this.$el.scroll(this.detect_scroll);
+			// debounce function that happens with scroll event
+			this.$el.scroll(_.debounce(this.detect_scroll, 80));
 		},
 
 		detect_scroll: function() {
@@ -137,18 +142,18 @@
 		}
 	});
 
-	var AppView = Backbone.View.extend({
+	mfTumblr.AppView = Backbone.View.extend({
 		initialize: function() {
 			var
 				scrollAmount = $(window).scrollTop(),
-				postsView = new PostsView(),
-				fullWindowView = new FullWindowView({
-					model: new FullWindow({
+				postsView = new mfTumblr.PostsView(),
+				fullWindowView = new mfTumblr.FullWindowView({
+					model: new mfTumblr.FullWindow({
 						scrollAmount: scrollAmount
 					})
 				}),
-				headerView = new HeaderView({
-					model: new Header()
+				headerView = new mfTumblr.HeaderView({
+					model: new mfTumblr.Header()
 				})
 			;
 
@@ -164,5 +169,6 @@
 		}
 	});
 
-	var appView = new AppView();
-})(jQuery);
+$(function() {
+	new mfTumblr.AppView();
+});
